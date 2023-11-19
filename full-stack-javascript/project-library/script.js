b66019077abc851a58b68998bc51009d4333c807
Justin Bento <bento.a.justin@gmail.com>
2023-11-19 17:45:22 -0500

    invoked a new function to call the titles

--- full-stack-javascript/project-library/script.js
+++ full-stack-javascript/project-library/script.js
@@ -17,6 +17,14 @@ function Book(title, author, numberOfPages, pagesRead) {
   };
 }
 
+const book1 = new Book("The Big Short", "Michael Lewis", 264, 137);
+const book2 = new Book("Atomic Habits", "James Clear", 253, 253);
+const book3 = new Book("Animal Farm", "George Orwell", 94, 94);
+const book4 = new Book("Brave New World", "Aldous Huxley", 235, 0); //235
+const book5 = new Book("What Every Body Is Saying", "Joe Navarro", 235, 0); //235
+const book6 = new Book("Writing For The Web", "Crawford Kilian", 187, 187);
+const book7 = new Book("The Design of Everyday Things", "Don Norman", 347, 347);
+
 function addBookToLibrary() {
   // do stuff here
 }
