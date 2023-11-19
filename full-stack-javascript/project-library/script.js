b0ec40a66512286030e15b9750c61ef313389df2
Justin Bento <bento.a.justin@gmail.com>
2023-11-19 17:55:21 -0500

    organized the code and dsiplayed books to library

--- full-stack-javascript/project-library/script.js
+++ full-stack-javascript/project-library/script.js
@@ -1,7 +1,8 @@
 const myLibrary = [];
+const collection = document.querySelector(".myLibrary");
 
 function Book(title, author, numberOfPages, pagesRead) {
-  // the constructor..
+  // Constructor
   this.title = title; 
   this.author = author;
   this.numberOfPages = numberOfPages;
@@ -17,18 +18,39 @@ function Book(title, author, numberOfPages, pagesRead) {
   };
 }
 
+function addBookToLibrary(book) {
+  myLibrary.push(book);
+}
+
+function createBookElement(book) {
+  const li = document.createElement('li');
+  li.textContent = `${book.title} by ${book.author} - ${book.status()}`;
+  return li;
+}
+
+function displayLibrary() {
+  const body = document.body;
+  const ul = document.createElement('ul');
+
+  myLibrary.forEach(book => {
+    const li = createBookElement(book);
+    ul.appendChild(li);
+  });
+
+  body.appendChild(ul);
+}
+
+// Sample books
 const book1 = new Book("The Big Short", "Michael Lewis", 264, 137);
 const book2 = new Book("Atomic Habits", "James Clear", 253, 253);
 const book3 = new Book("Animal Farm", "George Orwell", 94, 94);
-const book4 = new Book("Brave New World", "Aldous Huxley", 235, 0); //235
-const book5 = new Book("What Every Body Is Saying", "Joe Navarro", 235, 0); //235
+const book4 = new Book("Brave New World", "Aldous Huxley", 235, 0);
+const book5 = new Book("What Every Body Is Saying", "Joe Navarro", 235, 0);
 const book6 = new Book("Writing For The Web", "Crawford Kilian", 187, 187);
 const book7 = new Book("The Design of Everyday Things", "Don Norman", 347, 347);
 
-function addBookToLibrary(book) {
-  // do stuff here
-  myLibrary.push(book)
-}
+
+// Add books to the library
 addBookToLibrary(book1);
 addBookToLibrary(book2);
 addBookToLibrary(book3);
@@ -36,3 +58,6 @@ addBookToLibrary(book4);
 addBookToLibrary(book5);
 addBookToLibrary(book6);
 addBookToLibrary(book7);
+
+// Display the library
+displayLibrary();
