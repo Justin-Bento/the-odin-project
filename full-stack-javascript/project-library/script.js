3e5075ecab13c0c581376499cb1131391b313ad3
Justin Bento <bento.a.justin@gmail.com>
2023-11-19 17:44:59 -0500

    added paramaters for the book to give more descriptions

--- full-stack-javascript/project-library/script.js
+++ full-stack-javascript/project-library/script.js
@@ -1,7 +1,20 @@
 const myLibrary = [];
 
-function Book() {
-  // the constructor...
+function Book(title, author, numberOfPages, pagesRead) {
+  // the constructor..
+  this.title = title; 
+  this.author = author;
+  this.numberOfPages = numberOfPages;
+  this.pagesRead = pagesRead;
+  this.status = function() {
+    if (this.pagesRead && this.numberOfPages === this.pagesRead) {
+      return "completed";
+    } else if (this.pagesRead === 0) {
+      return "haven't read";
+    } else {
+      return "currently reading";
+    }
+  };
 }
 
 function addBookToLibrary() {
