a1a0cb674f330575ca548c9641433c1be13380ef
Justin Bento <bento.a.justin@gmail.com>
2023-12-11 16:26:29 -0500

    moved logic into click event listener

--- foundations/rock-paper-scissors/script.js
+++ foundations/rock-paper-scissors/script.js
@@ -1,45 +1,49 @@
-const conditions = {
-  rock: 'rock',
-  paper: 'paper',
-  scissors: 'scissors',
-};
-
-function playRound(playerSelection, computerSelection) {
-  playerSelection = playerSelection.toLowerCase();
-
-  const winningCombinations = [
-    ['rock', 'scissors'],
-    ['paper', 'rock'],
-    ['scissors', 'paper'],
-  ];
-
-  if (playerSelection === computerSelection) {
-    console.log(`It's a tie! ${playerSelection} can't beat ${computerSelection}.`);
-  } else if (winningCombinations.some(([player, computer]) => player === playerSelection && computer === computerSelection)) {
-    console.log(`${playerSelection} beats ${computerSelection}. You win!`);
-  } else {
-    console.log(`${computerSelection} beats ${playerSelection}. You lose!`);
+let startGame = document.querySelector(".startGame");
+startGame.addEventListener("click", (event) => {
+  const conditions = {
+    rock: 'rock',
+    paper: 'paper',
+    scissors: 'scissors',
+  };
+  
+  function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+  
+    const winningCombinations = [
+      ['rock', 'scissors'],
+      ['paper', 'rock'],
+      ['scissors', 'paper'],
+    ];
+  
+    if (playerSelection === computerSelection) {
+      console.log(`It's a tie! ${playerSelection} can't beat ${computerSelection}.`);
+    } else if (winningCombinations.some(([player, computer]) => player === playerSelection && computer === computerSelection)) {
+      console.log(`${playerSelection} beats ${computerSelection}. You win!`);
+    } else {
+      console.log(`${computerSelection} beats ${playerSelection}. You lose!`);
+    }
   }
-}
-
-function getComputerChoice(max) {
-  const decision = Math.floor(Math.random() * max);
-
-  if (decision === 0) {
-    return conditions.rock;
-  } else if (decision === 1) {
-    return conditions.paper;
-  } else {
-    return conditions.scissors;
+  
+  function getComputerChoice(max) {
+    const decision = Math.floor(Math.random() * max);
+  
+    if (decision === 0) {
+      return conditions.rock;
+    } else if (decision === 1) {
+      return conditions.paper;
+    } else {
+      return conditions.scissors;
+    }
   }
-}
+  
+  function getPlayerChoice() {
+    const playerChoice = window.prompt('Choose Rock, Paper, or Scissors:', '');
+    return playerChoice.toLowerCase();
+  }
+  
+  const playerSelection = getPlayerChoice();
+  const computerSelection = getComputerChoice(3);
+  event.preventDefault
+  playRound(playerSelection, computerSelection);
 
-function getPlayerChoice() {
-  const playerChoice = window.prompt('Choose Rock, Paper, or Scissors:', '');
-  return playerChoice.toLowerCase();
-}
-
-const playerSelection = getPlayerChoice();
-const computerSelection = getComputerChoice(3);
-
-playRound(playerSelection, computerSelection);
+})
