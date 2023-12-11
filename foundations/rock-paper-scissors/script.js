ef105d889ce58350f8ec0b9878750d6b132c166c
Justin Bento <bento.a.justin@gmail.com>
2023-12-11 15:29:05 -0500

    fixed errors within the code

--- foundations/rock-paper-scissors/script.js
+++ foundations/rock-paper-scissors/script.js
@@ -1,11 +1,27 @@
-function playRound(playerSelection, computerSelection) {
-  // your code here!
-}
- 
-const playerSelection = "rock";
+const playerSelection = getPlayerChoice("Rock");
 const computerSelection = getComputerChoice(3);
-console.log(computerSelection);
+
+function playRound(playerSelection, computerSelection) {
+  console.log(computerSelection + playerSelection);
+  // Add the logic for the game round here
+}
 
 function getComputerChoice(max) {
-  return Math.floor(Math.random() * max)
+  // Generate a random decision (0 for Rock, 1 for Paper, 2 for Scissors)
+  let decision = Math.floor(Math.random() * max);
+  // Determine the output based on the value of the 'decision' variable
+  if (decision === 0) {
+    return "Rock!";
+  } else if (decision === 1) {
+    return "Paper!";
+  } else if (decision === 2) {
+    return "Scissors!";
+  }
 }
+
+function getPlayerChoice(input) {
+  return `${input}`;
+}
+
+// Call the playRound function to log the result
+playRound(playerSelection, computerSelection);
