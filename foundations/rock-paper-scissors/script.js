090efc524ee5d9ded92b6ae049ab2492d5d1b97f
Justin Bento <bento.a.justin@gmail.com>
2023-12-11 15:00:24 -0500

    layed out code snippets given by the odin project

---
+++ foundations/rock-paper-scissors/script.js
@@ -0,0 +1,11 @@
+const playerSelection = "rock";
+const computerChoice = getComputerChoice()
+
+function playRound(playerSelection, computerSelcetion) {
+  // your code goes here
+}
+function game() {
+  // make a best-of-five game that keeps score and reports a winner or loser at the end.
+}
+
+console.log(playRound(playerSelection, computerSelection));
