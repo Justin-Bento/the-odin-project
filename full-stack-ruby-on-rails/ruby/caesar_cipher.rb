5e3dc51e34d7b7dafffd12d585e6110dff05836a
Justin Bento <bento.a.justin@gmail.com>
2024-03-15 23:47:46 -0400

    wrote pseudo-code for the caesar cipher

--- full-stack-ruby-on-rails/ruby/caesar_cipher.rb
+++ full-stack-ruby-on-rails/ruby/caesar_cipher.rb
@@ -3,3 +3,8 @@
 # Objective: create the cipher shift encryption and shift your message around.
 # Purpose: help solidify your knowledge and get warmed up to working on small projects.
 
+# Pseudo Code:
+# -----------------------------------------------------------------------------------------
+/bin/bash: line 1: :WQA: command not found
+# When the plaintext is grabbed from the encrypted text, it will shift by the proposed number. 
+# For example, in the case of 3, 'D' would be replaced by 'A', 'E' would become 'B', and so on.
