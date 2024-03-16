c6e6a0d3a78b666ecd51a75cc39488833e367074
Justin Bento <bento.a.justin@gmail.com>
2024-03-15 23:40:45 -0400

    created file metadata for me to refrence back

---
+++ full-stack-ruby-on-rails/ruby/caesar_cipher.rb
@@ -0,0 +1,5 @@
+# Author: Justin Bento
+# Start Date 2024-03-1
+# Objective: create the cipher shift encryption and shift your message around.
+# Purpose: help solidify your knowledge and get warmed up to working on small projects.
+
