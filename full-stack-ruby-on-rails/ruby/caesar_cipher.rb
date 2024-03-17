9d2bc6f85df8837bef80119bac0efe6d716226ad
Justin Bento <bento.a.justin@gmail.com>
2024-03-17 01:50:39 -0400

    Define the alphabet, Rotate the alphabets, Combine rotated alphabets into a single string

--- full-stack-ruby-on-rails/ruby/caesar_cipher.rb
+++ full-stack-ruby-on-rails/ruby/caesar_cipher.rb
@@ -9,10 +9,19 @@
 # When you get the message back, you shift each letter back by the same number of steps.
 # For instance, if you shift each letter by 3, 'D' would turn into 'A', 'E' would become 'B', and so on."
 
-def caesar_cipher()
-  # Define a string representing the alphabet
-  alphabet = ("a".."z").to_a.join + ("A".."Z").to_a.join
-  puts alphabet
+def caesar_cipher(shift)
+  # Define the alphabet
+  lowercase_alphabet = ("a".."z").to_a
+  uppercase_alphabet = ("A".."Z").to_a
+
+  # Rotate the alphabets
+  rotated_lowercase_alphabet = lowercase_alphabet.rotate(shift)
+  rotated_uppercase_alphabet = uppercase_alphabet.rotate(shift)
+
+  # Combine rotated alphabets into a single string
+  rotated_alphabet = rotated_lowercase_alphabet.join + rotated_uppercase_alphabet.join
+
+  return rotated_alphabet
 end
 
-puts caesar_cipher
+puts caesar_cipher(2)
