2964794831469dedb0c5d64a626c329fdb86c610
Justin Bento <bento.a.justin@gmail.com>
2024-03-17 01:38:56 -0400

    removed parameters for the caesar_cipher method

--- full-stack-ruby-on-rails/ruby/caesar_cipher.rb
+++ full-stack-ruby-on-rails/ruby/caesar_cipher.rb
@@ -9,7 +9,7 @@
 # When you get the message back, you shift each letter back by the same number of steps.
 # For instance, if you shift each letter by 3, 'D' would turn into 'A', 'E' would become 'B', and so on."
 
-def caesar_cipher(content, number)
+def caesar_cipher()
   # Define a string representing the alphabet
   #alphabet = ("a".."z").to_a.join + ("A".."Z").to_a.join
 end
