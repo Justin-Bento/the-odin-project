42671f7413055a876783e2b1f70c41c1f7688887
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2024-11-21 13:27:28 -0500

    Added create update read and delete book function

--- full-stack-javascript/project-library/script-class.js
+++ full-stack-javascript/project-library/script-class.js
@@ -2,4 +2,8 @@ class Library {
   constructor() {
     this.myLibrary = []; // Manage library as a class property
   }
+  addBookToLibrary(book) {}
+  createBookElement(book) {}
+  getBookStatus(book) {}
+  displayLibrary() {}
 }
