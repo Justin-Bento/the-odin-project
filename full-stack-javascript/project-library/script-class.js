af8621b1a064572321b15c3f055391e9a377e411
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2024-11-21 13:28:25 -0500

    Coppied code from functional and updated syntax to work alongside class layout

--- full-stack-javascript/project-library/script-class.js
+++ full-stack-javascript/project-library/script-class.js
@@ -2,8 +2,26 @@ class Library {
   constructor() {
     this.myLibrary = []; // Manage library as a class property
   }
-  addBookToLibrary(book) {}
-  createBookElement(book) {}
-  getBookStatus(book) {}
-  displayLibrary() {}
+  addBookToLibrary(book) {
+    this.myLibrary.push(book);
+  }
+  createBookElement(book) {
+    const li = document.createElement("li");
+    li.textContent = `${book.title} by ${book.author} - ${this.getBookStatus(book)}`;
+    return li;
+  }
+  getBookStatus(book) {
+    return book.pagesRead === book.numberOfPages ? "Completed" : `Reading (${book.pagesRead}/${book.numberOfPages})`;
+  }
+  displayLibrary() {
+    const body = document.body;
+    const ul = document.createElement("ul");
+
+    this.myLibrary.forEach((book) => {
+      const li = this.createBookElement(book);
+      ul.appendChild(li);
+    });
+
+    body.appendChild(ul);
+  }
 }
