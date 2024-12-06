dc89694783e49b519f6778090128863cfef88ef9
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2024-12-06 00:34:35 -0500

    imported stylesheet to main javascript file.

--- node-path-javascript-restaurant-page/src/index.js
+++ node-path-javascript-restaurant-page/src/index.js
@@ -1,6 +1,6 @@
 // Import the main_page function
 import { main_page } from "./main_page.js";
-
+import "./styles/style.css";
 // Call the function to render the content
 document.addEventListener("DOMContentLoaded", () => {
   main_page();
