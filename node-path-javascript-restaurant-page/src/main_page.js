d5afc8c4846e20ce88824efb3ffc661334678dd5
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2024-12-06 01:12:37 -0500

    Created a title to host the bistro huddy menu - Soups & Salads

--- node-path-javascript-restaurant-page/src/main_page.js
+++ node-path-javascript-restaurant-page/src/main_page.js
@@ -8,10 +8,14 @@ export const main_page = () => {
   const headline = document.createElement("h1");
   const supporting = document.createElement("p");
   const title = document.createElement("h2");
+  const subtitle_appetizers = document.createElement("h3");
+  const subtitle_SoupsSalads = document.createElement("h3");
 
   // Section for writing Content
   headline.textContent = "Welcome To Bistro Huddy";
   title.textContent = "Bistro Huddy Menu";
+  subtitle_appetizers.textContent = "Appetizers";
+  subtitle_SoupsSalads.textContent = "Soups & Salads";
 
   supporting.textContent =
     "Bistro Huddy is a fictional restaurant and set created by Drew Talbert and his wife, Andrea Kelley.";
@@ -20,4 +24,6 @@ export const main_page = () => {
   content.appendChild(headline);
   content.appendChild(supporting);
   content.appendChild(title);
+  content.appendChild(subtitle_appetizers);
+  content.appendChild(subtitle_SoupsSalads);
 };
